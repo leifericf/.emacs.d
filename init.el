4257781a7b9cfb1fe7c96acd972987478e228b0b
Leif Eric Fredheim <mutableif@users.noreply.github.com>
2022-03-11 16:16:47 +0100

    Deleted borked submodule

--- init.el
+++ init.el
@@ -6,17 +6,17 @@
 (load custom-file)
 
 ;; Define paths to dependencies
-(setq site-lisp-dir
-      (expand-file-name "site-lisp" user-emacs-directory))
+(setq vendor-dir
+      (expand-file-name "vendor" user-emacs-directory))
 (setq settings-dir
   (expand-file-name "settings" user-emacs-directory))
 
 ;; Add dependencies to load path
 (add-to-list 'load-path settings-dir)
-(add-to-list 'load-path site-lisp-dir)
+(add-to-list 'load-path vendor-dir)
 
 ;; Add external dependencies (Git submodules) to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
+(dolist (project (directory-files vendor-dir t "\\w+"))
   (when (file-directory-p project)
     (add-to-list 'load-path project)))
 
